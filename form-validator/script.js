d295b9eafe892118a7e98b5cc8fe4badedc8e049
311298 <kashyapanurag109@gmail.com>
2022-09-01 15:22:50 +0530

    done with form validator

--- form-validator/script.js
+++ form-validator/script.js
@@ -24,20 +24,38 @@ function showSuccess(input){
 
 // validator for email
 // copy paste from stack overflow
-function isValidEmail(email){
+function checkEmail(input){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase())
+    if(re.test(input.value.trim())){
+        showSuccess(input)
+    }else{
+        showError(input,'Email is not valid')
+    }
 }
 
 // check required fields
 function checkRequired(inputArr){
     inputArr.forEach(function(input){
         if(input.value.trim() === ''){
-            showError(input,`${getFieldName(input)} is required`)
+            showError(
+                input,
+                `${getFieldName(input)} is required`)
         }else{
             showSuccess(input)
         }
     })
+}
+
+// check input length
+function checkLength(input,min,max){
+    if(input.value.length < min){
+        showError(input,`${getFieldName(input)} must be atleast ${min} characters`)
+
+    }else if(input.value.length > max){
+        showError(input,`${getFieldName(input)} must be less than ${max} characters`)
+    }else{
+        showSuccess(input)
+    }
 }
 
 //get field name
@@ -45,9 +63,20 @@ function getFieldName(input){
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
+// check password match
+function checkPasswordMatch(input1, input2){
+    if(input1.value !== input2.value){
+        showError(input2,'Passwords do not match')
+    }
+}
+
 //event listener
 form.addEventListener('submit',function(e){
     e.preventDefault() // it does not automatically refresh each time
 
     checkRequired([username,email,password,password2]);
+    checkLength(username,3,15)
+    checkLength(password,7,25)
+    checkEmail(email)
+    checkPasswordMatch(password,password2)
 })
