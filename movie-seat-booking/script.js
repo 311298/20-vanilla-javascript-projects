fe346ab31162fdcc94d193122a8e1bd128404129
311298 <kashyapanurag109@gmail.com>
2022-09-02 14:09:13 +0530

    local storage logic

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -10,6 +10,19 @@ let ticketPrice = +movieSelect.value;
 function updateSelectCount(){
     const selectedSeats = document.querySelectorAll('.row .seat.selected') // give space between row and seat as it is child of row
     // console.log(selectedSeats)
+
+    // to select the index of each seats
+    // copy selected seats into arr
+    // map through arr
+    // return the new arr with indexes
+    const seatIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat))
+    // console.log(seatIndex)
+
+    // save it local storage
+    localStorage.setItem('selectedSeats',JSON.stringify(seatIndex))
+
+
+
     count.innerText= selectedSeats.length // for changing the number represented for selected seats
     total.innerText = ticketPrice * selectedSeats.length
 }
@@ -17,8 +30,21 @@ function updateSelectCount(){
 // movie changed event
 movieSelect.addEventListener('change',(e)=>{
     ticketPrice = +e.target.value // here we are changing the ticket price from select
+
+    // to select the index of flim options
+    // console.log(e.target.selectedIndex) // selectedIndex is in built property
+
+    setMovieData(e.target.selectedIndex,e.target.value)
+
     updateSelectCount()
 })
+
+// save selected movie index and price
+function setMovieData(movieIndex,moviePrice){
+    localStorage.setItem('selectMovieIndex',movieIndex)
+    localStorage.setItem('selectMoviePrice',moviePrice)
+    // no json stringfy as data is already in string format
+}
 
 // seat clicked event
 container.addEventListener('click',(e)=>{
